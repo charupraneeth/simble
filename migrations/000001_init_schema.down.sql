c78d04f8ffc144b377223b9c7b1840db052304a0
charupraneeth <charupraneeth@gmail.com>
2026-03-29 15:59:34 +0530

    Add inital sql migration scripts

---
+++ migrations/000001_init_schema.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_analytics_path;
+DROP INDEX IF EXISTS idx_analytics_visitor_site;
+DROP INDEX IF EXISTS idx_analytics_site_date;
+
+DROP TABLE IF EXISTS analytics;
+
+DROP TABLE IF EXISTS sites;
---
+++ migrations/000001_init_schema.up.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS sites (
+    id SERIAL PRIMARY KEY,
+    domain varchar(255) UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+) ;
+
+create table if not exists analytics(
+	id BIGINT primary key,
+	site_id integer references sites(id) on delete cascade not null,
+	
+	visitor_id varchar(16) not null,
+	
+	path text not null default '/',
+	
+	browser_name text,
+	device_type varchar(255),
+	os_name text,
+	
+	country_code char(2),
+	city_name text,
+	
+	created_at timestamptz default NOW() not null
+) ;
+
+create index idx_analytics_site_date on analytics (site_id, created_at desc) ;
+
+CREATE INDEX idx_analytics_visitor_site ON analytics (site_id, visitor_id, created_at) ;
