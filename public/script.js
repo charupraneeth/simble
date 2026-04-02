7ae38036d7e1c2bf791692bf09f5cf8e7a260360
charupraneeth <charupraneeth@gmail.com>
2026-04-02 15:26:09 +0530

    Update tracking script

--- public/script.js
+++ public/script.js
@@ -1,19 +1,22 @@
-// 1. Get the current script tag and extract the data-domain attribute
-const scriptTag = document.currentScript;
-const domain = scriptTag.getAttribute('data-domain');
+(function () {
+    // 1. Get the current script tag and extract the data-domain attribute
+    const scriptTag = document.currentScript;
+    const domain = scriptTag.getAttribute('data-domain');
 
-// 2. Send the analytics hit
-fetch("https://simble.up.railway.app/api/event", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "domain": domain,
-        "name": "pageview",
-        "referer": document.referrer || null,
-        "url": window.location.href
-    })
-}).catch((err) => {
-    // Fail silently so we don't spam the website's console
-});
+    // 2. Send the analytics hit
+    fetch("https://simble.up.railway.app/api/event", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "domain": domain,
+            "name": "pageview",
+            "referer": document.referrer || null,
+            "url": window.location.href
+        })
+    }).catch((err) => {
+        // Fail silently so we don't spam the website's console
+    });
+
+})()
